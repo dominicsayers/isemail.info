c78e6f186db341a4c076059261eda0fc8b0e0bdd
Dominic Sayers <dominic@sayers.cc>
2016-12-05 10:53:44 +0000

    Update Disqus widget

--- web/index.php
+++ web/index.php
@@ -545,6 +545,7 @@ if ($mime !== 'text/html') {
 	<![endif]-->
 
 	<!-- Disqus -->
+	<!--
 	<script>
 		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 		var disqus_shortname = 'isemail-info'; // required
@@ -560,6 +561,23 @@ if ($mime !== 'text/html') {
 			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 		})();
 	</script>
+	-->
+
+	<!-- New Disqus -->
+	<script>
+		var disqus_config = function () {
+			this.page.url = 'https://isemail.info';
+			this.page.identifier = 'isemail.info';
+		};
+
+		(function() { // DON'T EDIT BELOW THIS LINE
+		var d = document, s = d.createElement('script');
+		s.src = '//isemail-info.disqus.com/embed.js';
+		s.setAttribute('data-timestamp', +new Date());
+		(d.head || d.body).appendChild(s);
+		})();
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
 	<!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet
 	     change the UA-XXXXX-X to be your site's ID -->
